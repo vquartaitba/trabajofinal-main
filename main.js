4e5fcf2829811876dfd7663ca48b741bf1af470f
vquarta <110499464+vquartaitba@users.noreply.github.com>
2022-11-21 18:11:18 -0300

    vamos avanzando

--- main.js
+++ main.js
@@ -83,6 +83,7 @@ generateShop();
 let increment = (id) => {
   let selectedItem = id;
   let search = basket.find((x) => x.id === selectedItem.id);
+
   if (search === undefined) {
     basket.push({
       id: selectedItem.id,
@@ -91,23 +92,35 @@ let increment = (id) => {
   } else {
     search.item += 1;
   }
-   update(selectedItem.id);
- };
 
-
- let decrement = (id) => {
+  // console.log(basket);
+  update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+let decrement = (id) => {
   let selectedItem = id;
   let search = basket.find((x) => x.id === selectedItem.id);
+
   if (search === undefined) return;
   else if (search.item === 0) return;
   else {
     search.item -= 1;
   }
- update(selectedItem.id);
- };
-
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  // console.log(basket);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
 let update = (id) => {
   let search = basket.find((x) => x.id === id);
+  // console.log(search.item);
   document.getElementById(id).innerHTML = search.item;
   calculation();
-};  
+};
+
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+calculation();
